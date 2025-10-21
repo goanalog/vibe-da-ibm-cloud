e5f763a83330b3f81dbdab447b0fa8fadaaa7e3c
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-21 16:15:10 -0500

    a

--- main.tf
+++ main.tf
@@ -37,7 +37,13 @@ resource "ibm_resource_instance" "cos_instance" {
   plan              = "lite"
   location          = var.region
   resource_group_id = data.ibm_resource_group.group.id
-  tags              = ["vibe", "deployable-architecture", "static-website"]
+
+  tags = [
+    "vibe",
+    "deployable-architecture",
+    "static-website",
+    "ibm-cloud"
+  ]
 }
 
 # --- COS Bucket ---
@@ -55,11 +61,12 @@ resource "ibm_cos_bucket" "bucket" {
   }
 }
 
-# --- Upload HTML (either inline or sample) ---
+# --- Inline or Local HTML ---
 locals {
   index_html = var.index_html != "" ? var.index_html : file("${path.module}/index.html")
 }
 
+# --- Upload HTML to COS ---
 resource "ibm_cos_bucket_object" "index_html" {
   bucket_crn = ibm_cos_bucket.bucket.crn
   key        = "index.html"
@@ -67,50 +74,3 @@ resource "ibm_cos_bucket_object" "index_html" {
   etag       = md5(local.index_html)
 }
 
-# --- Outputs ---
-output "vibe_bucket_url" {
-  description = "Direct link to your sacred bucket."
-  value       = "https://s3.${var.region}.cloud-object-storage.appdomain.cloud/${ibm_cos_bucket.bucket.bucket_name}/"
-}
-
-output "vibe_url" {
-  description = "Behold the consecrated endpoint for direct vibe consumption."
-  value       = "https://s3.${var.region}.cloud-object-storage.appdomain.cloud/${ibm_cos_bucket.bucket.bucket_name}/index.html"
-}
-
-# --- Variables ---
-variable "region" {
-  description = "IBM Cloud region (e.g., us-south)."
-  type        = string
-  default     = "us-south"
-}
-
-variable "resource_group" {
-  description = "IBM Cloud resource group name."
-  type        = string
-  default     = "default"
-}
-
-variable "cos_instance_name" {
-  description = "Friendly name for your IBM Cloud Object Storage instance."
-  type        = string
-  default     = "vibe-coder-cos"
-}
-
-variable "bucket_name" {
-  description = "Base name for your COS bucket (lowercase, no spaces)."
-  type        = string
-  default     = "vibe-coder-sample-bucket"
-}
-
-variable "index_html" {
-  description = "Inline HTML code pasted by the user."
-  type        = string
-  default     = ""
-}
-
-variable "public_access" {
-  description = "Whether to make the bucket publicly readable (recommended for hosting)."
-  type        = bool
-  default     = true
-}
