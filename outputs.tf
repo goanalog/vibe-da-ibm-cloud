af1cce1a842c4b1721f5c8569d637efa353f3a13
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-21 18:49:58 -0500

    asdf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vibe_url" {
-  description = "Public URL of your vibe-coded app."
-  value       = "https://${ibm_cos_bucket.bucket.bucket_name}.s3.${var.region}.cloud-object-storage.appdomain.cloud/index.html"
-}
-
-output "vibe_bucket_url" {
-  description = "URL of your Cloud Object Storage bucket."
-  value       = "https://${ibm_cos_bucket.bucket.bucket_name}.s3.${var.region}.cloud-object-storage.appdomain.cloud"
-}
