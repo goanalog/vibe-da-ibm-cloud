ad9e505d66fb1d8883ec9264186bc4ef6c3417c6
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-21 14:17:00 -0500

    asdf

--- main.tf
+++ main.tf
@@ -65,36 +65,39 @@ resource "ibm_cos_bucket" "bucket" {
   region_location      = var.region
   storage_class        = "standard"
   force_delete         = true
+  
+  # --- FIX 2: Set public access directly on the bucket ---
+  public_access        = true
 }
 
 # ---------------------------------------------------------------------
 # Upload the index.html (inline or default)
 # ---------------------------------------------------------------------
-# --- FIXED: Changed from "ibm_cos_object" to "ibm_cos_bucket_object" ---
 resource "ibm_cos_bucket_object" "index_html" {
   bucket       = ibm_cos_bucket.bucket.bucket_name
   key          = "index.html"
   content      = local.html_content
   content_type = "text/html"
+  
+  # Ensure bucket is created and public before uploading
+  depends_on = [ibm_cos_bucket.bucket] 
 }
 
 # ---------------------------------------------------------------------
 # Upload the vibe-face.png asset
 # ---------------------------------------------------------------------
-# --- FIXED: Changed from "ibm_cos_object" to "ibm_cos_bucket_object" ---
 resource "ibm_cos_bucket_object" "vibe_face" {
   bucket       = ibm_cos_bucket.bucket.bucket_name
   key          = "vibe-face.png"
-  source       = "${path.module}/vibe-face.png"
+  
+  # --- FIX 1: Changed "source" to "file" ---
+  file         = "${path.module}/vibe-face.png"
   content_type = "image/png"
+  
+  # Ensure bucket is created and public before uploading
+  depends_on = [ibm_cos_bucket.bucket]
 }
 
 # ---------------------------------------------------------------------
-# Make the bucket public
+# (The old public access resource block has been deleted)
 # ---------------------------------------------------------------------
-# --- FIXED: Replaced "ibm_cos_bucket_policy" with "ibm_cos_bucket_public_access" ---
-resource "ibm_cos_bucket_public_access" "public_access" {
-  bucket_name          = ibm_cos_bucket.bucket.bucket_name
-  resource_instance_id = ibm_resource_instance.cos_instance.id
-  public_access        = "public-read"
-}
