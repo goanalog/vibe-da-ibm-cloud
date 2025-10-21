97ead2eaff73081d757a866149fc55559581cec6
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-21 15:07:21 -0500

    clean

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vibe_url" {
-  description = "Behold the consecrated endpoint for direct vibe consumption."
-  value       = "https://${ibm_cos_bucket.bucket.bucket_name}.s3.${var.region}.cloud-object-storage.appdomain.cloud/index.html"
-}
-
-output "vibe_bucket_url" {
-  description = "Direct link to your sacred bucket."
-  value       = "https://${ibm_cos_bucket.bucket.bucket_name}.s3.${var.region}.cloud-object-storage.appdomain.cloud/"
-}
