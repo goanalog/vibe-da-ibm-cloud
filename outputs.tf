2d03e6740d8831c1ffe422972686b771226de996
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-21 21:03:48 -0500

    zoink

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "vibe_url" {
   description = "Public URL of your vibe-coded app."
-  value       = "https://${ibm_cos_bucket.bucket.bucket_name}.s3.${var.region}.cloud-object-storage.appdomain.cloud/index.html"
+  value       = "https://s3.${ibm_cos_bucket.bucket.region_location}.cloud-object-storage.appdomain.cloud/${ibm_cos_bucket.bucket.bucket_name}/index.html"
 }
 
 output "vibe_bucket_url" {
   description = "URL of your Cloud Object Storage bucket."
-  value       = "https://${ibm_cos_bucket.bucket.bucket_name}.s3.${var.region}.cloud-object-storage.appdomain.cloud"
+  value       = "https://s3.${ibm_cos_bucket.bucket.region_location}.cloud-object-storage.appdomain.cloud/${ibm_cos_bucket.bucket.bucket_name}/"
 }
