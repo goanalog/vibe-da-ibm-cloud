c6c5184fe028f63fa30d0a544e89955bf402b94a
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-21 19:46:43 -0500

    Fix COS object configuration for public deploy

--- main.tf
+++ main.tf
@@ -1,23 +1,8 @@
-terraform {
-  required_providers {
-    ibm = {
-      source  = "IBM-Cloud/ibm"
-      version = ">= 1.84.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.0.0"
-    }
-  }
-}
-
-provider "ibm" {}
-
 resource "random_string" "suffix" {
   length  = 6
   lower   = true
   upper   = false
-  number  = true
+  numeric = true
   special = false
 }
 
@@ -45,9 +30,10 @@ resource "ibm_cos_bucket" "bucket" {
 
 resource "ibm_cos_bucket_object" "index_html" {
   bucket_crn      = ibm_cos_bucket.bucket.crn
-  bucket_location = var.region
+  bucket_location = ibm_cos_bucket.bucket.region_location
   key             = "index.html"
   content         = var.html_input != "" ? var.html_input : file("index.html")
   endpoint_type   = "public"
   force_delete    = true
+  acl             = "public-read"
 }
