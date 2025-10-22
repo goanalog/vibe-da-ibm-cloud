f44d299d5658aff6e5cb10e7ddf09393e4e68a89
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-21 19:38:47 -0500

    Ensure public COS bucket link works out of the box

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    ibm = {
-      source  = "IBM-Cloud/ibm"
-      version = ">= 1.84.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.0.0"
-    }
-  }
-}
-
-provider "ibm" {}
-
 resource "random_string" "suffix" {
   length  = 6
   lower   = true
@@ -45,9 +30,25 @@ resource "ibm_cos_bucket" "bucket" {
 
 resource "ibm_cos_bucket_object" "index_html" {
   bucket_crn      = ibm_cos_bucket.bucket.crn
-  bucket_location = var.region
+  bucket_location = ibm_cos_bucket.bucket.region_location
   key             = "index.html"
   content         = var.html_input != "" ? var.html_input : file("index.html")
+  content_type    = "text/html; charset=utf-8"
   endpoint_type   = "public"
   force_delete    = true
+}
+
+resource "ibm_cos_bucket_policy" "public_read" {
+  bucket_crn      = ibm_cos_bucket.bucket.crn
+  bucket_location = ibm_cos_bucket.bucket.region_location
+  policy = jsonencode({
+    Version   = "2.0"
+    Statement = [{
+      Sid       = "PublicReadGetObject"
+      Effect    = "Allow"
+      Principal = { AWS = ["*"] }
+      Action    = ["s3:GetObject"]
+      Resource  = ["${ibm_cos_bucket.bucket.crn}/*"]
+    }]
+  })
 }
