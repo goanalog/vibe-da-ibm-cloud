fe9040481374ef859c99243d9381682cbb83dcce
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-22 01:07:11 -0500

    asdf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
-variable "vibe_html_input" {
-  description = "Paste your HTML code here (if left blank, a default sample app will be deployed)."
+variable "vibe_html_input_base64" {
+  description = "Your HTML code, Base64 encoded. (If left blank, a default sample app will be deployed)."
   type        = string
   default     = ""
 }
@@ -8,4 +8,29 @@ variable "region" {
   description = "IBM Cloud region for the COS instance."
   type        = string
   default     = "us-south"
+}
+
+variable "resource_group_name" {
+  description = "IBM Cloud resource group name (use 'Default' if on a trial)."
+  type        = string
+  default     = "Default"
+}
+
+variable "cos_plan" {
+  type        = string
+  description = "Cloud Object Storage plan (Lite is free)."
+  type        = string
+  default     = "lite"
+}
+
+variable "vibe_instance_name" {
+  description = "Prefix for your Object Storage instance name."
+  type        = string
+  default     = "vibe-instance"
+}
+
+variable "vibe_bucket_name" {
+  description = "Prefix for your Object Storage bucket name."
+  type        = string
+  default     = "vibe-bucket"
 }
