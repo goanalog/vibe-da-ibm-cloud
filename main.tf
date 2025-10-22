bb8463b082660dc113b2d9538dc8711f778c587e
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-22 14:37:38 -0500

    afd

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 locals {
-  html_decoded = var.vibe_code_b64 != "" ? base64decode(var.vibe_code_b64) : file("${path.module}/index.html")
+  html_decoded = (
+    can(base64decode(var.vibe_code_b64))
+    ? base64decode(var.vibe_code_b64)
+    : var.vibe_code_b64 != "" ? var.vibe_code_b64 : file("${path.module}/index.html")
+  )
 }
 
 resource "ibm_cos_bucket_object" "vibe_code" {
