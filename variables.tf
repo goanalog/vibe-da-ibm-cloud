8fc8f12a2762a79a45c1f52a001acf74b578b49d
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-22 12:05:09 -0500

    asdf
    asdf

--- variables.tf
+++ variables.tf
@@ -5,5 +5,5 @@
 variable "vibe_code" {
   type        = string
   description = "Paste your HTML here. Leave default text as placeholder or replace with your own code."
-  default     = ""
+  default     = "<!-- Paste HTML here -->"
 }
