a4c8e4076bd0cb46e201fd49f183f6029b846d91
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-22 13:50:01 -0500

    adf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-#############################
-# Variables
-#############################
-
-variable "vibe_code" {
-  type        = string
-  description = "Paste your HTML code here. This replaces the sample index.html when you deploy."
-  default     = "Paste your HTML code here"
-}
