dd53a796a0c6f4dcddc1d7c8c17dde13412107e5
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-23 18:47:59 -0500

    df

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  description = "IBM Cloud region for resources and COS bucket."
+  type        = string
+  default     = "us-south"
+}
+
+variable "resource_group" {
+  description = "IBM Cloud resource group name."
+  type        = string
+  default     = "Default"
+}
+
+variable "bucket_prefix" {
+  description = "Prefix for the public website bucket."
+  type        = string
+  default     = "vibe-bucket"
+}
+
+variable "enable_functions" {
+  description = "Whether to deploy Cloud Functions (push_to_cos / push_to_project)."
+  type        = bool
+  default     = true
+}
