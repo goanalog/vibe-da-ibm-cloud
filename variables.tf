7f0fd82ab83d3906a49b4fc09c8efdb4797f0a55
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-23 13:24:19 -0500

    dsf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  type        = string
+  description = "IBM Cloud region"
+  default     = "us-south"
+}
+
+variable "resource_group" {
+  type        = string
+  description = "IBM Cloud resource group"
+  default     = "Default"
+}
