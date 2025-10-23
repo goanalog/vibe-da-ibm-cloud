4ed0d17edc3376562a3485bacbd6ab8d5c625052
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-23 08:49:16 -0500

    adsf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # main.tf
 
-[cite_start]provider "ibm" {} [cite: 4]
+provider "ibm" {}
 
 resource "random_string" "suffix" {
   length  = 6
@@ -10,7 +10,7 @@ resource "random_string" "suffix" {
 
 locals {
   html_content = var.vibe_code_raw != "" ?
-  [cite_start]var.vibe_code_raw : file("${path.module}/index.html") [cite: 5]
+  var.vibe_code_raw : file("${path.module}/index.html")
 }
 
 resource "ibm_resource_instance" "vibe_instance" {
@@ -32,13 +32,10 @@ resource "ibm_cos_bucket_object" "vibe_code" {
   bucket_crn      = ibm_cos_bucket.vibe_bucket.crn
   bucket_location = ibm_cos_bucket.vibe_bucket.region_location
 
-  [cite_start]key     = "index.html" [cite: 6]
+  key     = "index.html"
   content = local.html_content
   etag    = md5(local.html_content)
   
   # This makes the individual object public, fixing the 403 error
   acl = "public-read"
 }
-
-# The ibm_iam_access_group_policy resource has been removed 
-# as it conflicts with the object-level 'acl' setting.
