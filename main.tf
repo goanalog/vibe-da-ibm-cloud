dd2e9c98a3ead2a067297f2584743588bd0d66e8
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-22 23:01:52 -0500

    asdf

--- main.tf
+++ main.tf
@@ -25,42 +25,16 @@ resource "ibm_cos_bucket" "vibe_bucket" {
   storage_class        = "standard"
   region_location      = "us-south"
   force_delete         = true
-  
-  # --- ROLLING BACK PREVIOUS FIX ---
-  # public_access = true is not supported, so it is removed.
-  # --- END OF ROLLBACK ---
 }
 
-# --- REMOVED INVALID RESOURCE ---
-# The resource "ibm_cos_bucket_public_access" was here,
-# but it is not valid in this provider version.
-# --- END OF REMOVAL ---
-
 resource "ibm_cos_bucket_object" "vibe_code" {
-  # --- FIX based on provider error log ---
-  # Replaced 'bucket' with 'bucket_crn' and added required 'bucket_location'
-  # to match the provider's expected arguments.
-  bucket_crn      = ibm_cos_bucket.vibe_bucket.crn
-  bucket_location = ibm_cos_bucket.vibe_bucket.region_location
-  # --- END OF FIX ---
-  
-  # --- NEW FIX HERE ---
-  # Setting the acl (Access Control List) to "public-read" on the
-  # object itself. This should make this specific file public.
-  acl = "public-read"
-  # --- END OF FIX ---
-
+  bucket  = ibm_cos_bucket.vibe_bucket.bucket_name
   key     = "index.html"
   content = local.html_decoded
   etag    = md5(local.html_decoded)
-
-  # Removed the depends_on for the invalid resource.
-  # The dependencies on ibm_cos_bucket.crn and ibm_cos_bucket.region_location
-  # are now implicit and sufficient.
 }
 
 output "vibe_url" {
   value       = "https://s3.us-south.cloud-object-storage.appdomain.cloud/${ibm_cos_bucket.vibe_bucket.bucket_name}/index.html"
   description = "Public URL for your deployed Vibe app"
 }
-
