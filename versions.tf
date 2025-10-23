7c537c2626b2b9a0786301f7da279f795b529865
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-23 00:17:46 -0500

    usb-midi

--- versions.tf
+++ versions.tf
@@ -2,12 +2,15 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      # Keep the version that works for your environment
-      version = ">= 1.84.0" 
+      # Require a newer provider that supports ibm_cos_bucket_public_access
+      version = ">= 1.85.0" 
     }
     random = {
       source  = "hashicorp/random"
       version = ">= 3.0.0"
     }
+    # No longer need the null provider
   }
+  # Optional: Enforce the Terraform engine version (will error if Schematics ignores catalog.json)
+  # required_version = ">= 1.5.0" 
 }
