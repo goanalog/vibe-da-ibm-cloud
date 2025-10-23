1bd64b0cd1417ede08da7de45f0f7b567ddd20d6
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-23 12:43:49 -0500

    dff

--- main.tf
+++ main.tf
@@ -49,13 +49,16 @@ resource "ibm_cos_bucket" "bucket" {
 
 # Static website configuration
 resource "ibm_cos_bucket_website_configuration" "bucket_website" {
-  bucket_name     = ibm_cos_bucket.bucket.bucket_name
-  bucket_location = ibm_cos_bucket.bucket.region_location
-
+  bucket_crn = ibm_cos_bucket.bucket.crn
   website_configuration {
-    main_document  = "index.html"
-    error_document = "404.html"
+    main_document {
+      suffix = "index.html"
+    }
+    error_document {
+      key = "404.html"
+    }
   }
+}
 }
 # IBM Cloud Functions namespace & package
 resource "ibm_function_namespace" "ns" {
@@ -96,24 +99,24 @@ locals {
   push_project_url = "${local.functions_base}/${ibm_function_action.push_to_project.name}"
 }
 
-
 # Upload index.html with live URLs injected
 resource "ibm_cos_bucket_object" "index_html" {
-  bucket_name     = ibm_cos_bucket.bucket.bucket_name
-  bucket_location = ibm_cos_bucket.bucket.region_location
+  bucket_crn = ibm_cos_bucket.bucket.crn
+  
 key          = "index.html"
   content      = base64encode(templatefile("${path.module
 }/samples/index.html", {
     PUSH_COS_URL     = local.push_cos_url,
     PUSH_PROJECT_URL = local.push_project_url
-  }))
+  ,
+    URL = local.vibe_url}))
   content_type = "text/html"
 }
 
 # Upload 404 fallback page
 resource "ibm_cos_bucket_object" "page_404" {
-  bucket_name     = ibm_cos_bucket.bucket.bucket_name
-  bucket_location = ibm_cos_bucket.bucket.region_location
+  bucket_crn = ibm_cos_bucket.bucket.crn
+  
 key          = "404.html"
   content      = base64encode(file("${path.module
 }/samples/404.html"))
