2a9e005c5924bd53394209d2742ed388f1f04da1
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-22 22:04:29 -0500

    asdf

--- main.tf
+++ main.tf
@@ -27,14 +27,40 @@ resource "ibm_cos_bucket" "vibe_bucket" {
   force_delete         = true
 }
 
+# --- CRITICAL FIX ADDED HERE ---
+# This resource makes the bucket's contents readable by the public.
+# Without this, the vibe_url will return a 403 Access Denied error.
+resource "ibm_cos_bucket_public_access" "vibe_bucket_public_access" {
+  bucket_name   = ibm_cos_bucket.vibe_bucket.bucket_name
+  public_access = "public-read"
+  
+  # We must ensure this happens *after* the bucket is created
+  depends_on = [
+    ibm_cos_bucket.vibe_bucket
+  ]
+}
+# --- END OF FIX ---
+
 resource "ibm_cos_bucket_object" "vibe_code" {
-  bucket  = ibm_cos_bucket.vibe_bucket.bucket_name
+  # --- FIX based on provider error log ---
+  # Replaced 'bucket' with 'bucket_crn' and added required 'bucket_location'
+  # to match the provider's expected arguments.
+  bucket_crn      = ibm_cos_bucket.vibe_bucket.crn
+  bucket_location = ibm_cos_bucket.vibe_bucket.region_location
+  # --- END OF FIX ---
+  
   key     = "index.html"
   content = local.html_decoded
   etag    = md5(local.html_decoded)
+
+  # We must ensure this happens *after* the public access is set
+  depends_on = [
+    ibm_cos_bucket_public_access.vibe_bucket_public_access
+  ]
 }
 
 output "vibe_url" {
   value       = "https://s3.us-south.cloud-object-storage.appdomain.cloud/${ibm_cos_bucket.vibe_bucket.bucket_name}/index.html"
   description = "Public URL for your deployed Vibe app"
 }
+
