16015a5d7f8dbf27663baa5cb159be6a3ac979d0
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-23 13:41:33 -0500

    sdf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  description = "IBM Cloud region for COS bucket (e.g., us-south)."
-  type        = string
-}
-
-variable "resource_group" {
-  description = "IBM Cloud resource group name."
-  type        = string
-}
